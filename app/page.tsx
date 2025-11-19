c44f887b3dcbf2778bbd183c8e55bc3530dfaf54
Luca Fumagalli <fumagalli.lcu@gmail.com>
2025-11-19 20:10:58 +0100

    Added prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
-export default Home
+  return (
+    <div>
+      <h1 className="text-light-500 bg-black text-2xl">Welcome to nextjs</h1>
+    </div>
+  );
+};
+export default Home;
